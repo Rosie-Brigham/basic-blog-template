fd661ed7422d3d617db9d3658a9c28ebb37b0666
Roo <rosiebrigham@gmail.com>
2017-04-13 11:00:02 +0100

    finishin up

--- scripts.js
+++ scripts.js
@@ -8,8 +8,16 @@ core.switchCompare = function() {
   $('.thumbnail').on('click', function(e) {
     e.preventDefault();
     var src = e.target.src
+    var caption = e.target.nextElementSibling.innerHTML
 
     var img = $('#original-image')[0]
+    var oCaption = $('#modified')[0]
+
     img.src = src
+    oCaption.innerHTML = caption
+    var id = e.target.id
+
+    var section = $('.' + id)[0]
+    $(section).css('display', 'block')
 	});
 }

4d009afbdb304cc2235650b28fd5bc3dbe8ce7cb
Roo <rosiebrigham@gmail.com>
2017-04-18 15:38:05 +0100

    tweaks

--- scripts.js
+++ scripts.js
@@ -7,22 +7,22 @@ window.onload = function() {
 core.switchCompare = function() {
   $('.thumbnail').on('click', function(e) {
     e.preventDefault();
+    console.log('fired')
     if ($('.onShow').length > 0 ) {
       $('.onShow').removeClass('onShow')
     }
 
-    var src = e.target.src
-    var caption = e.target.nextElementSibling.innerHTML
+    var src = $(this).find('img')[0].src
+    var caption = $(this).find('figcaption')[0].innerHTML
 
     var img = $('#original-image')[0]
     var oCaption = $('#modified')[0]
 
     img.src = src
     oCaption.innerHTML = caption
-    var id = e.target.id
+    var id = $(this).parent()[0].id
 
     var section = $('.' + id)[0]
-    // $(section).css('display', 'block')
     $(section).toggleClass('onShow')
 	});
 }
